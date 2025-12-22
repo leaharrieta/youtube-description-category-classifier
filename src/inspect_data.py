a5724ca8b6adbca79f7d2f6380386446f9683354
Leah Arrieta <leaha3@umbc.edu>
2025-12-22 16:36:47 -0500

    Updated inspect_data.py with exception handling.

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -1,19 +1,30 @@
+# import pandas library and remane to pd
 import pandas as pd
 
-df = pd.read_csv(
-    "data/youtube_video_dataset.csv",
-    encoding="utf-8",
-    engine="python"
-)
+# Read the csv file and convert into a data frame
+file = "data/youtube_video_dataset.csv"
 
-print("Dataset shape:", df.shape)
-print("\nColumns:")
-print(df.columns)
+try:
+    df = pd.read_csv(
+        file,
+        encoding = "utf-8", # To handle global text (emojis, special chars)
+        engine = "python")  # To handle multiline text
+    
+    print("Dataframe successfully created from file.")
+    print("Dataset Shape:", df.shape)   # makes (rows, cols)
+    print("\nColoums:")
+    print(df.columns)
 
-print("\nSample rows:\n")
-for i in range(3):
-    print("TITLE:")
-    print(df.iloc[i]["Title"])
-    print("\nDESCRIPTION (first 300 chars):")
-    print(df.iloc[i]["Description"][:300])
-    print("\n" + "-"*50 + "\n")
+    # Print the first three data entries
+    print("\nSample Rows:")
+    for i in range(3):
+        print("\nTITLE:")
+        print(df.iloc[i]["Title"])  # iloc (integer location) give row at location i
+        print("\nDESCRIPTION (first 300 chars):")
+        print(df.iloc[i]["Description"][:300])
+        print("\n" + "-" * 50 + "\n")   # For readability
+
+except FileNotFoundError:
+    print(f"File '{file}' not found")
+except Exception as e:
+    print(f"An error occured {e}.")
