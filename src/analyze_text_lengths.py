9b5f5ab3dee4d3af49448ceb13e8d395980a6e12
Leah Arrieta <leaha3@umbc.edu>
2025-12-23 15:54:26 -0500

    Implemented analyze_text_lengths and found the average title length is 56 char, min = 8 char , and max = 101 char. Titles are generally short and consistent with no extreme outliers. Therefore, moving forward, titles will not be truncated. The description length has a mean = 1250 char, median = 854 char, 75% = 1873, and max = 5000 char. The descriptions vary greatly with some being very long (links, spam, promos). This is verified in the top three longest descriptions the file printed out. In the future, long descriptions will not be kept and descriptions will be truncated to a fixed maximum length of 1024 characters.

---
+++ src/analyze_text_lengths.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+file = "data/youtube_video_dataset.csv"
+
+def main():
+    df = pd.read_csv(
+        file,
+        encoding = "utf-8",
+        engine = "python"
+        )
+    print("Total entries: ", df.shape[0])   # Number of rows
+
+    # Make a new coloum in df for num of chars in each title 
+    # and another for num of chars in each description
+    df["title_length"] = df["Title"].str.len()  # Process as a string and get length
+    df["des_length"] = df["Description"].str.len()
+
+    # Get summary stats on the now numeric data
+    print(df["title_length"].describe())
+    print(df["des_length"].describe())
+
+    # Compute cut points to later choose a cut off
+    percentiles = df["des_length"].quantile([0.90, 0.95, 0.99])
+
+    # How many extreme longs exist?
+    long_2000 = (df["des_length"] > 2000).sum() # Returns total num of true
+    long_3000 = (df["des_length"] > 3000).sum()
+
+    # Get examples of long descriptions (only 3 largest)
+    long_example = df.sort_values("des_length", ascending= False).head(3)
+
+    # Iterate through each row of long_example and print
+    for _, row in long_example.iterrows():
+        print("\nTitle:", row["Title"])
+        print("Description length: ", row["des_length"])
+        print(row["Description"][:300], "...")
+
+if __name__ == "__main__":
+    main()
