14f568f68aa823c67b5fc316c39f5130faaef431
Leah Arrieta <leaha3@umbc.edu>
2025-12-26 18:54:46 -0500

    Updated src/clean_data.py with the function remove_url to further clean descriptions in preparation for modeling.

--- src/clean_data.py
+++ src/clean_data.py
@@ -5,6 +5,7 @@ INPUT_FILE = "data/youtube_video_dataset.csv"
 OUTPUT_FILE = "data/cleaned_dataset.csv"
 MAX_LENGTH = 1024
 
+# def remove_emoji() - Function to remove emojis and other characters from text
 def remove_emoji(text):
     # Check if input is a string
     if not isinstance(text, str):
@@ -25,6 +26,17 @@ def remove_emoji(text):
     # Replace emojis with an empty string
     return emoji_pattern.sub("", text)
 
+# def remove_url() - Function to remove URLs from text
+def remove_url(text):
+    if not isinstance(text, str):
+        return ""
+    
+    url_pattern = re.compile(
+        r"(http\S+|www\S+)",
+        flags = re.IGNORECASE   # Case insensitive
+    )
+    return url_pattern.sub("", text)
+
 def main():
     df = pd.read_csv(
         INPUT_FILE,
@@ -36,14 +48,26 @@ def main():
 
     # Remove missing values
     df = df.dropna(subset = ["Title", "Description"])
+    
     # Strip whitespace
     df["Title"] = df["Title"].str.strip()
     df["Description"] = df["Description"].str.strip()
+
+    # Remove URLs
+    df["Title"] = df["Title"].apply(remove_url)
+    df["Description"] = df["Description"].apply(remove_url)
+
     # Remove emojis
     df["Title"] = df["Title"].apply(remove_emoji)
     df["Description"] = df["Description"].apply(remove_emoji)
+
+    # Remove leftover whitespace
+    #df["Title"] = df["Title"].str.replace(r"\s+", " ", regex=True).str.strip()
+    #df["Description"] = df["Description"].str.replace(r"\s+", " ", regex=True).str.strip()
+
     # Truncate desctiptions
     df["Description"] = df["Description"].str.slice(0, MAX_LENGTH)
+
     # Save clean dataset
     df.to_csv(OUTPUT_FILE, index = False)   # Do not add row name index
 
