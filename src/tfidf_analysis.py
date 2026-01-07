e38305ea18cb72bf0b609fe95e8714a842150e7e
Leah Arrieta <leaha3@umbc.edu>
2026-01-06 21:50:32 -0500

    Implemented tfidf_analysis.py which performed TF-IDF analysis on cleaned_dataset.csv. The cleaned data was split by Category and TF-IDF identified the most distinct words in each and gave a score. This data reveals category specific language and next tasks will be to prepare the dataset for NLP and ML tasks.

---
+++ src/tfidf_analysis.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer # To extract in a format supported by ML (text->numerical)
+
+INPUT_FILE = "data/cleaned_dataset.csv"
+
+def main():
+    # Load cleaned dataset
+    df = pd.read_csv(INPUT_FILE)
+    print("Total rows: ", len(df))
+    # Get the unique values from the Category coloumn
+    print("Categories: ", df["Category"].unique())
+
+    # Valid strings only
+    df = df.dropna(subset=["Description"])
+
+    # Make a category document for each category, saving only its corresponding description.
+    # Also join each category description into one string with a space in between
+    category_doc = df.groupby("Category")["Description"].apply(" ".join)
+
+    # Construct an object that keeps a max of 5000 words and removes english stop words
+    vectorizer = TfidfVectorizer(max_features = 500, stop_words = "english")
+    # Learn from the vocab data, compute TF-IDF scores produce a matrix
+    tfidf_matrix = vectorizer.fit_transform(category_doc)
+    # Return words that correspond to TF-IDF coloumns
+    feature_names = vectorizer.get_feature_names_out()
+
+    print("\nTop TF-IDF words per category:")
+    for index, category in enumerate(category_doc.index):
+        # Make row a list-like array of TF-IDF scores for a category across all features
+        row = tfidf_matrix[index].toarray()[0]
+        # Get the 10 most important words (largest -> smallest)
+        top_indices = row.argsort()[-10:][::-1]
+        print(f"\nCategory: {category}")
+
+        for i in top_indices:
+            # Print each categories most distinctive word and its TF-IDF score
+            print(f"{feature_names[i]:15} {row[i]:.4f}")
+
+if __name__ == "__main__":
+    main()
