6936a01091a37b30f4ecddecb262626a356bc9ee
Leah Arrieta <leaha3@umbc.edu>
2026-01-10 19:02:46 -0500

    Modified category_classifier.py to use TF-IDF to train a model. Using the previous train_test_split, TF-IDF was used to convert text into numeric form and Logistic Regression used to train the model. Accuracy scores and a classification report is the output. The model has an overall accuracy of 90 percent. Meaning around 90 percent of the time the model correctly predict the category of a description it has never seen before. The greatest precision is the Food category while the lowest is the History category. The F1 scores mainly range from 0.89-0.90 which indicates a good balance between precision and recall. Also, with the support for all categories being above 300, the number of test samples make the data meaningful. Lastly, the macro average at 0.90 indicates the model is consistent and the weighted average also at 0.90 indicates no class dominates. The equal values prove the dataset is balanced. Therefore, the model is solid and can be further developed.

--- src/category_classifier.py
+++ src/category_classifier.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 INPUT_FILE = "data/cleaned_dataset.csv"
 
@@ -10,8 +13,8 @@ def main():
 
     print("Total rows: ", len(df))
 
-    X = df["Description"]   # Input features (to learn from)
-    y = df["Category"]      # Labels (correct category for each description)
+    X = df["Description"]   # Input features (what the model learns from)
+    y = df["Category"]      # Labels (what the model is trying to predict)
 
     # 80% train, 20% test
     X_train, X_test, y_train, y_test = train_test_split(
@@ -23,6 +26,26 @@ def main():
 
     print("Train size: ", len(X_train))
     print("Test size: ", len(X_test))
+
+    # TF-IDF
+    vectorizer = TfidfVectorizer(max_features = 5000, stop_words = "english")
+    # Learn vocab from training data + convert text to numbers
+    X_train_tfidf = vectorizer.fit_transform(X_train)
+    X_test_tfidf = vectorizer.transform(X_test)
+
+    # Classifier
+    model = LogisticRegression(max_iter = 1000) # Higher chance to find a good solution
+    # Make the model map the numerical descriptions to the correct categories
+    model.fit(X_train_tfidf, y_train)
+
+    # Predictions
+    y_pred = model.predict(X_test_tfidf)
+
+    # Evaluation
+    # Compare predictions to the true answers
+    print("Accuracy score: ", accuracy_score(y_test, y_pred))
+    print("Classification report:\n")
+    print(classification_report(y_test, y_pred))
 
     # To spot class imbalance
     print("Training category counts: ")
